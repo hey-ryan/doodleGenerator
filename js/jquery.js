621a4b465c8404704154e9010334908bff02024f
Ryan Carranco <rjoncarranco@gmail.com>
2020-04-11 10:35:10 -0500

    update 1
    added styles

---
+++ js/jquery.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".button").click(function(){
+        $("p").css({"background-color": "#eee", "margin": "10px",
+        "overflow": "hidden", "padding": "5px", "border-radius":
+        "3px", "border": "1px solid #999", "width": "auto",
+        "width": "200px"});
+    });
+});
